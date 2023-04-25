d190ce609cbaddc056c7aa4f3e8fb3625a789118
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2023-04-24 21:13:59 -0400

    Nueva web el faro con bootstrap v.1.1

--- fecha-hora.js
+++ fecha-hora.js
@@ -7,7 +7,7 @@ const fechaActual = new Date();
 
 const dia = fechaActual.getDate();
 
-const mes = fechaActual.getMonth();
+const mes = fechaActual.getMonth() + 1;
 
 const año = fechaActual.getFullYear();
 
